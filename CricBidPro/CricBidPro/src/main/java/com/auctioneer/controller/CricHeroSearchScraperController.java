8a22805cbfd8e0694ccfee04da6b0f4f1de8253d
ChiranjeeviH <45878605+ChiranjeeviH@users.noreply.github.com>
2023-11-04 11:15:51 +0530

    scrapper fix and enhancement

--- CricBidPro/CricBidPro/src/main/java/com/auctioneer/controller/CricHeroSearchScraperController.java
+++ CricBidPro/CricBidPro/src/main/java/com/auctioneer/controller/CricHeroSearchScraperController.java
@@ -1,48 +1,45 @@
 package com.auctioneer.controller;
 
 import com.auctioneer.service.CricHeroSearchScraper;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/scrapper")
 public class CricHeroSearchScraperController {
+	private final CricHeroSearchScraper cricHeroSearchScraper;
 
-    private final CricHeroSearchScraper cricHeroSearchScraper;
-
-  private static String API_ENDPOINT = "https://cricheroes.in/api/v1/other/global-search/";	
+  private final String API_ENDPOINT = "https://cricheroes.in/api/v1/other/global-search/";
 
     public CricHeroSearchScraperController(CricHeroSearchScraper cricHeroSearchScraper) {
         this.cricHeroSearchScraper = cricHeroSearchScraper;
     }
 
-    @GetMapping("/fetch-data")
-    public ResponseEntity<Map<String, Object>> fetchDataFromURL() {
-        return cricHeroSearchScraper.getMainpageResponse();
+    @GetMapping("/fetchuserinfo/{cricheroName}")
+    public Mono<String> fetchDataFromURL(@PathVariable String cricheroName) {
+        return cricHeroSearchScraper.getUserInfo(cricheroName);
     }
 
    
 
 	@GetMapping("/callApi/{search}")
 	public String callApi(@PathVariable String search) throws IOException, InterruptedException {
-		API_ENDPOINT = API_ENDPOINT + search + "?pagesize=12";
+		String url = API_ENDPOINT + URLEncoder.encode(search, StandardCharsets.UTF_8) + "?pagesize=12";
 		HttpClient client = HttpClient.newHttpClient();
 
-		HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(API_ENDPOINT))
+		HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url))
 				.header("Api-Key", "cr!CkH3r0s").header("Device-Type", "Chrome: 119.0.0.0")
 				.header("Udid", "ab7905ae4e2ddf11cc91ca0e05241cfc")
 
--- CricBidPro/CricBidPro/src/main/java/com/auctioneer/service/CricHeroSearchScraper.java
+++ CricBidPro/CricBidPro/src/main/java/com/auctioneer/service/CricHeroSearchScraper.java
@@ -1,15 +1,14 @@
 package com.auctioneer.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class CricHeroSearchScraper {
@@ -21,19 +20,14 @@ public class CricHeroSearchScraper {
         this.webClient = webClientBuilder.baseUrl("https://cricheroes.in").build();
     }
 
-    public ResponseEntity<Map<String, Object>> getMainpageResponse() {
-        String searchEndpoint = "/api/v1/other/global-search/chiranjeevi%20haridasula?pagesize=12";
+    public Mono<String> getUserInfo(String cricheroName) {
+        String searchEndpoint = "/api/v1/other/global-search/"+ URLEncoder.encode(cricheroName, StandardCharsets.UTF_8)+"?pagesize=12";
 
-        ParameterizedTypeReference<Map<String, Object>> responseType = new ParameterizedTypeReference<>() {
-        };
-        ResponseEntity<Map<String, Object>> responseEntity = this.webClient.get()
+        return this.webClient.get()
                 .uri(searchEndpoint)
                 .headers(httpHeaders -> httpHeaders.addAll(buildCustomHeaders()))
                 .retrieve()
-                .toEntity(responseType)
-                        .block();
-
-        return ResponseEntity.ok(Objects.requireNonNull(responseEntity).getBody());
+                .bodyToMono(String.class);
     }
 
     private HttpHeaders buildCustomHeaders() {
@@ -43,11 +37,9 @@ public class CricHeroSearchScraper {
         headers.add("Accept-Language", "en-US,en;q=0.5");
         headers.add("api-key", "cr!CkH3r0s");
         headers.add("Connection", "keep-alive");
-        //headers.add("Cookie", "_gcl_au=1.1.1676337311.1697974295; _ga_RHRT76MSXD=GS1.1.1698771995.4.1.1698772059.60.0.0; ..."); // Add the full Cookie value
-        headers.add("device-type", "Firefox: 119.0");
+        headers.add("Device-Type", "Firefox: 119.0");
         headers.add("Host", "cricheroes.in");
         headers.add("If-None-Match", "W/\"19e-5ZNG9WT/UmbdtjoW4WvDwg\"");
-        headers.add("Referer", "https://cricheroes.in/search?q=chiranjeevi+haridasula");
         headers.add("Sec-Fetch-Dest", "empty");
         headers.add("Sec-Fetch-Mode", "cors");
         headers.add("Sec-Fetch-Site", "same-origin");
@@ -64,7 +56,8 @@ public class CricHeroSearchScraper {
 
     // Method to build the Cookie header value with the provided cookies
     private String buildCookieHeaderValue() {
-        String cookieHeaderValue = String.join("; ", List.of(
+
+        return String.join("; ", List.of(
                 "FCNEC=[[\"AKsRol_bHBq4WhRa87GAdJCLoshHwEl6ue8ZpiSGIPXm8kOZelGIlku8xpVfbGdZt2Gxz23bFcFnJU0wXAVfeXU4YgVVpaqjFpbq4l8LAeyvDeeOQPPDjNqHPy7xfeC82lVauSBgmbeoylGwI9-7lf0ffHM7DYUCFg==\"],null,[]]",
                 "__gads=ID=8f03e9b63159bc25-22c0cd917a8000d1:T=1697974294:RT=1698859033:S=ALNI_MZLukZo_TtDlfLXQLJ5VsD4uP0nEA",
                 "__gpi=UID=00000c6d3fd324b1:T=1697974294:RT=1698859033:S=ALNI_Mb51hJh0Ul_093st06FbcqRSe4-Ng",
@@ -76,14 +69,8 @@ public class CricHeroSearchScraper {
                 "_ga_2MNGBJB3NG=GS1.1.1698859045.5.1.1698859149.60.0.0",
                 "_ga_RHRT76MSXD=GS1.1.1698859045.5.1.1698859149.60.0.0",
                 "_gcl_au=1.1.1676337311.1697974295",
-                "browserId=d4bb255d919e5ecca984ca460aeffa0d",
-                "connect.sid=s:3r7u6237uGx9OdbElPH66kQNLzsZLVhg.e9brcynr6oPKyufaE/4a5g4KARlgHVypswD8DJ6FL5A",
-                "sharedid=24432233-0a88-4428-8401-e232a05e85d4",
-                "sharedid_cst=TyylLI8srA==",
                 "udid=bcf417562c89e0bc96136dd10c325926"
         ));
-
-        return cookieHeaderValue;
     }
 
 }
